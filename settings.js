32bcdd36c6c43ef86ba36e028d0aba8babcb5a4f
PashaDIE <85843949+PashaDIE@users.noreply.github.com>
2023-07-23 13:49:48 -0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,71 @@
+
+/**
+  * Created by Maulana / Lana
+  * Base : Dittaz & Irfan Haryanto
+  * Contact me on WhatsApp wa.me/6281775445373
+  * Subscribe me on Youtube : https://youtube.com/@maulanabot
+  
+  * If you want to buy an updated script that is not encrypted, please WhatsApp me
+*/
+
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
+const version = require("baileys/package.json").version 
+global.require = require
+global.language = "id"
+global.nomerOwner ="628983784623"// ganti sesuka hati lo
+global.nomerOwner2 ="628983784623"// ganti sesuka hati lo
+global.runWith = "Replit"
+global.ownerName = "Lana Botz"// ganti sesuka hati lo
+global.botName = "Lanabotz" // ganti sesuka hati lo
+global.sessionName ="session" //untuk session rm- rf session
+global.docType = "docx" // ga ush di ganti
+global.Qoted = "ftoko" // ga ush di ganti
+global.autoBio = true // tanya lana kalo mau aktifin auto bio
+global.baileysMd = true
+global.antiSpam = true
+//global.multi = true
+//global.prefa = "!"
+global.fake = botName
+global.Console = false
+global.copyright = `lanDev`
+global.baileysVersion = `baileys ${version}`
+global.fake1 ="LanaBotz"
+global.packName = "Lanabotz"
+global.authorName = "LanTeam"
+global.replyType = "web"
+global.setwelcome = "type2"
+global.autoblockcmd = false
+global.autoReport = true
+global.autoLevel = true
+global.autoSticker = false // true untuk kirim foto otomatis jd stiker
+global.gamewaktu = 60
+global.limitCount = 30 // untuk limit user
+global.Intervalmsg = 1000 //detik
+//===================//
+
+/** 
+ NOTE : 
+ 
+ * APIKEY BELI SENDIRI BANG *
+ * GAK ADA APIKEY?? FITUR BOTZ GAK BERJALAN *
+ 
+*/
+
+/*==========《 API Key 》============*/
+
+// TEMPAT NARO API FITUR  LOLHUMAN
+global.apikey =" " 
+
+
+/*==========《 API Key 》============*/
+
+
+/**
+  * Created by Maulana / Lana
+  * Base : Dittaz & Irfan Haryanto
+  * Contact me on WhatsApp wa.me/6281775445373
+  * Subscribe me on Youtube : https://youtube.com/@maulanabot
+  
+  * If you want to buy an updated script that is not encrypted, please WhatsApp me
+*/
